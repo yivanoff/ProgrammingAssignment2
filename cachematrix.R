2ef95f19dc4b56ca2db85ab1957dc48dde666090
yivanoff <yuri.ivanov@gmail.com>
2015-03-21 10:06:59 -0400

    Removed comment

--- cachematrix.R
+++ cachematrix.R
@@ -18,8 +18,6 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     s <- x$getsolve()

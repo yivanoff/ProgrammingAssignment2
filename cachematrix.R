29cfbd0516ba1405eb185ef9f2d06fb875ec9d06
yivanoff <yuri.ivanov@gmail.com>
2015-03-22 10:52:34 -0400

    Added chek of cacheSolve() parameter

--- cachematrix.R
+++ cachematrix.R
@@ -13,13 +13,23 @@ makeCacheMatrix <- function(x = matrix()) {
          getsolve = getsolve)
 }
 
-# Calculates or gets from cache inversion matrix to x which should be makeCacheMatrix class
+# Calculates or gets from cache inversion matrix to x which should be a makeCacheMatrix class
 cacheSolve <- function(x, ...) {
+    
+    # Check x parameter for having "getsolve()" method
+    lapply (names (makeCacheMatrix()),
+        function (i) if (! i %in% names (x)) 
+            stop (paste ("Wrong argument in cacheSolve() function. It must have ", i, "() method", ""))
+    )
+    
+    # Check if x has cached inversion matrix
     s <- x$getsolve()
     if(!is.null(s)) {
         message("getting cached data")
         return(s)
     }
+    
+    # If x doesn't have cached value then calculate it and save
     data <- x$get()
     s <- solve(data, ...)
     x$setsolve(s)
